85c2441f8f7a394a8eea23c39149f516b986e133
Tal Zahavi <tal.zahavi9@gmail.com>
2015-05-19 21:35:20 +0300

    Added save picture

--- app/src/main/java/com/anyonecan/tal/drawnumbersgame/DrawActivity.java
+++ app/src/main/java/com/anyonecan/tal/drawnumbersgame/DrawActivity.java
@@ -1,17 +1,38 @@
 package com.anyonecan.tal.drawnumbersgame;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
-public class DrawActivity extends ActionBarActivity {
+public class DrawActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button sendButton;
+    DrawingView drawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+
+        drawView = (DrawingView) findViewById(R.id.drawing);
+        sendButton = (Button) findViewById(R.id.btn_send);
+        sendButton.setOnClickListener(this);
     }
 
     @Override
@@ -35,4 +56,62 @@ public class DrawActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClick(View view) {
+        
+        if (view.getId() == R.id.btn_send) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                Log.d("AnyOneCan","Error creating media file, check storage permissions: ");
+                Toast.makeText(getApplicationContext(),"Error creating media file, check storage permissions: ",
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                drawView.setDrawingCacheEnabled(true);
+                Bitmap b = drawView.getDrawingCache();
+                b.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d("AnyOneCan", "File not found: " + e.getMessage());
+                Toast.makeText(getApplicationContext(),"File not found: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Log.d("AnyOneCan", "Error accessing file: " + e.getMessage());
+                Toast.makeText(getApplicationContext(),"Error accessing file:" + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+
+
+
+
+    /** Create a File for saving an image or video */
+    private  File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + getApplicationContext().getPackageName()
+                + "/Files");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName="AnyOneCan_"+ timeStamp +".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
+    }
+
 }
